3fb982b1eef7f734f1ab489e034d166e44fc27a7
Tejendra Khatri <khatrite@mnstate.edu>
2020-05-31 11:52:53 -0500

    Initial Commit

---
+++ Program 6/carTest.py
@@ -0,0 +1,28 @@
+'''
+This program tests the class Car and the getters and the setters contained within.
+'''
+__author__ = "Tejendra Khatri"
+__date__ = "Oct 1, 2017"
+
+from modCar import * 
+car1 = Car("Hyundai" , 2010, 45000)
+car2 = Car("Chevrolet", 2017, 89000)
+print("Details of car1:");print(car1,"\n")
+print("Details of car2:");print(car2,"\n")
+print("Testing getters for car1:")
+model = car1.getBrand()  
+print("Retrieved Model Name:",model)
+madeYear = car1.getMakeYear()
+print("Retrieved Make Year:",madeYear)
+price = car1.getPrice()
+print("Retrieved Price:", price)
+print("\nTesting setters for car2:")
+car2.setBrand("Toyota") #changing the brand name of car 2
+print("Changed the brand name from Chevrolet to :", car2.getBrand())
+car2.setMakeYear(1995) #changing the make year of car 2
+print("Changed the make year from 2017 to:", car2.getMakeYear())
+car2.setPrice(25000) #changing the price of car 2
+print("Changed the price from 89000 to:",car2.getPrice())
+print("\nUpdated details of car2:");print(car2)
+
+
---
+++ Program 6/modCar.py
@@ -0,0 +1,35 @@
+'''
+This program consists of a class called Car which has three attributes 
+brand name, make year and price of the car.
+'''
+
+__author__ = "Tejendra Khatri"
+__date__ = "Oct 1, 2017"
+
+class Car:
+    def __init__(self, tmpBrandName, tmpMakeYear, tmpPrice): #constructor
+        self.__brand = tmpBrandName
+        self.__makeYear = tmpMakeYear
+        self.__price = tmpPrice
+
+    def __str__(self):
+        return "Brand:"+self.__brand + "\n" + "Make Year:"+str(self.__makeYear) +\
+               "\n" + "Price:"+str(self.__price)
+
+    def getBrand(self): #getter for brand name
+        return self.__brand
+
+    def setBrand(self,newBrandName): #setter for brand name
+        self.__brand = newBrandName
+
+    def getMakeYear(self): #getter for make year
+        return self.__makeYear
+
+    def setMakeYear(self,newMakeYear):#setter for make year
+        self.__makeYear = newMakeYear
+
+    def getPrice(self):  #getter for price
+        return self.__price
+
+    def setPrice(self, newPrice):#setter for price
+        self.__price = newPrice
